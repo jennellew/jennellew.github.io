4c320c3c557b478961b4ebc43351338e1ee0142c
jennellew <jwongdev@gmail.com>
2016-09-12 16:01:08 -0400

    added lara + revamp'd slide js

--- assets/js/carousel.js
+++ assets/js/carousel.js
@@ -1,32 +1,58 @@
 $(function() {
+  var $t = $(this);
   var $reel      = $('.reel');
-  var $galleryimg = $('#main #gallery-img a')
+  var $galleryimg = $('#main #gallery-img a');
   var $gallerytitle = $('#main h2');
   var $gallerypara1 = $('#main header #paragraph-1');
   var $gallerypara2 = $('#main header #paragraph-2');
   var $gallerybutton = $('#main footer a');
 
- 
-  $reel.delegate('article', 'click', function(e) {
-    var $item = $(this).find('img')
-    var imgsrc = $item.data('alt');
-    var title = $item.data('title');
-    var paragraph1 = $item.data('paragraph1');
-    var paragraph2 = $item.data('paragraph2');
-    var href = $item.data('href');
-    var label = $item.data('label');
+  $('.reel article').on('click', updateText);
+
+  function updateText() {
+    var $item = $(this).find('img'),
+        imgsrc = $item.data('alt'),
+        title = $item.data('title'),
+        paragraph1 = $item.attr("data-paragraph1"),
+        paragraph2 = $item.attr("data-paragraph2"),
+        href = $item.data('href'),
+        label = $item.data('label'),
+        link = $item.data('link');
 
     $galleryimg.children('img').attr('src',imgsrc);
     $gallerytitle.empty().text( title );
-    $gallerypara1.empty().text( paragraph1 );
+    $gallerypara1.empty().text( paragraph1 );;
     $gallerypara2.empty().text( paragraph2 );
+    
+    // Append hyperlinks in text
+    if (link) {
+      var textToParse, field; 
+        if (paragraph1.indexOf('^') > -1) {
+          textToParse = paragraph1;
+          field = $gallerypara1;
+        } else if (paragraph2.indexOf('^') > -1) {
+          textToParse = paragraph2;
+          field = $gallerypara2;
+        }
 
+        var linkedText = $('<a />');
+        linkedText.attr("href", link);
+      
+        var parse = textToParse.split('^');
+        linkedText.text(parse[1]);
+        field.empty().append(parse[0]).append(linkedText).append(parse[2]);
+    }
+
+    // Show button
     if (href) {
       $gallerybutton.show().attr('href', href);
       $gallerybutton.empty().text(label);
     }
     else
       $gallerybutton.hide().attr('href', '');
+    };
 
-  })
+  $(window).load(function() {
+    $('.reel article')[0].click();
+  });
 });
--- assets/js/slider.js
+++ assets/js/slider.js
@@ -1,17 +1,47 @@
-var slideIndex = 1;
-showDivs(slideIndex);
+// var slideIndex = 1;
+// showDivs(slideIndex);
 
-function plusDivs(n) {
-  showDivs(slideIndex += n);
-}
+// function plusDivs(n) {
+//   showDivs(slideIndex += n);
+// }
 
-function showDivs(n) {
-  var i;
-  var x = document.getElementsByClassName("mySlides");
-  if (n > x.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = x.length} ;
-  for (i = 0; i < x.length; i++) {
-     x[i].style.display = "none";  
+// function showDivs(n) {
+//   var i;
+//   var x = document.getElementsByClassName("mySlides");
+//   if (n > x.length) {slideIndex = 1}    
+//   if (n < 1) {slideIndex = x.length} ;
+//   for (i = 0; i < x.length; i++) {
+//      x[i].style.display = "none";  
+//   }
+//   x[slideIndex-1].style.display = "block";  
+// }
+
+$('.mySlides').each(function() {
+  var $x = $(this),
+      $forward = $x.children(".btn-floating.right"),
+      $backward = $x.children(".btn-floating.left"),
+      $slideChildren = $x.children("[href]"),
+      length = $slideChildren.length,
+      slideIndex = 1;
+
+  showDivs(slideIndex);
+
+  $forward.on('click', function(){plusDivs(1)});
+  $backward.on('click', function(){plusDivs(-1)});
+
+  function plusDivs(n) {
+    showDivs(slideIndex += n);
   }
-  x[slideIndex-1].style.display = "block";  
-}
+
+  function showDivs(n) {
+    
+    //
+    var i;
+    if (n > length) {slideIndex = 1}    
+    if (n < 1) {slideIndex = length} ;
+    for (i = 0; i < length; i++) {
+      $slideChildren[i].style.display = "none"; 
+    }
+    $slideChildren[slideIndex-1].style.display = "block";
+  }
+}); 
